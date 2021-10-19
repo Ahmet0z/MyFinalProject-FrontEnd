c9be65dde1945b5a8c39476ef65018b5c284a102
Ahmet0z <l9120016@std.yildiz.edu.tr>
2021-10-19 14:59:50 +0300

    19. Ders Sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,6 +3,8 @@ import { Product } from 'src/app/models/product';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from 'src/app/services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product',
@@ -12,9 +14,13 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
+  filterText =""
+
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -39,6 +45,11 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     });
+  }
+
+  addToCart(product:Product){
+    this.cartService.addToCart(product);
+    this.toastrService.success(product.productName,"Sepete eklendi")
   }
 
 }
